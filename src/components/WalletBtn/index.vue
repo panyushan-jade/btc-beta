ca850a96ca382083e18404a671148b3ed32dffb5
panyushan <1193345664@qq.com>
2024-04-25 11:13:23 +0800

    feat：choose wallet

--- src/components/WalletBtn/index.vue
+++ src/components/WalletBtn/index.vue
@@ -1,18 +1,22 @@
 <script setup lang="ts">
 import { useAppStore } from '@/store/appStore';
 import { plusStar } from '@/utils/tools';
-
+import { ref } from 'vue';
+import OKX from '@/assets/img/okx.svg';
+import UNISAT from '@/assets/img/unisat.svg';
 const appStore = useAppStore();
 
+const walletDialogVisible = ref(false)
 const loadLink = ref(false);
 /**
  * 连接钱包
  */
 async function handleLink() {
-  if (loadLink.value) return;
-  loadLink.value = true;
-  await appStore.linkWallet();
-  loadLink.value = false;
+  walletDialogVisible.value = true
+  // if (loadLink.value) return;
+  // loadLink.value = true;
+  // await appStore.linkWallet();
+  // loadLink.value = false;
 }
 </script>
 
@@ -28,7 +32,28 @@ async function handleLink() {
       CONNECT WALLET
     </button>
   </div>
+  <el-dialog
+    v-model="walletDialogVisible"
+    title="Choose Wallet"
+    class="bg-#2b2b2b rounded-10"
+    width="500"
+    align-center
+  >
+    <template #title>
+      <div class="text-white text-30">CHOOSE WALLET</div>
+    </template>
+    
+    <button class="connect-btn font-scr w-full flex items-center mb-20 p-20 border-1 border border-#979797 rounded-10 bg-#414141" @click="handleLink">
+      <img :src="OKX" />
+      <span class="text-white text-20 ml-120">OKX WALLET</span>
+    </button>
+    <button class="connect-btn font-scr w-full flex items-center p-16 pl-20 border-1 border border-#979797 rounded-10 bg-#414141" @click="handleLink">
+      <img :src="UNISAT" />
+      <span class="text-white text-20 ml-105">UNISAT WALLET</span>
+    </button>
+  </el-dialog>
 </template>
+
 
 <style lang="scss" scoped>
 .wallet-wrap {
