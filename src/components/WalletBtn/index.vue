9263ca0e97675fb4d061e68b2d18bd5bc2595118
panyushan <1193345664@qq.com>
2024-04-27 11:50:46 +0800

    pref：connect wallet新ui

--- src/components/WalletBtn/index.vue
+++ src/components/WalletBtn/index.vue
@@ -2,17 +2,69 @@
 import { useAppStore } from '@/store/appStore';
 import { plusStar } from '@/utils/tools';
 import { ref } from 'vue';
+import { ElMessageBox } from 'element-plus'
+
 import OKX from '@/assets/img/okx.svg';
 import UNISAT from '@/assets/img/unisat.svg';
 const appStore = useAppStore();
-
-const walletDialogVisible = ref(false)
+const connectDialogVisible = ref(false);
+const installDialogVisible = ref(false);
 const loadLink = ref(false);
+
+const walletList = ref([
+  {
+    id: 1,
+    name: 'UNISAT WALLET',
+    src: OKX,
+  },
+  {
+    id: 2,
+    name: 'XVERSE WALLET',
+    src: OKX,
+  },
+  {
+    id: 3,
+    name: 'LEATHER WALLET',
+    src: OKX,
+  },
+  {
+    id: 4,
+    name: 'OKX WALLET',
+    src: OKX,
+  },
+  {
+    id: 5,
+    name: 'BIGGET WALLET',
+    src: OKX,
+  },
+  {
+    id: 6,
+    name: 'PHANTON WALLET',
+    src: OKX,
+  },
+  {
+    id: 7,
+    name: 'MAGIO EDEN',
+    src: OKX,
+  },
+  {
+    id: 8,
+    name: 'ENKRYRT',
+    src: OKX,
+  },
+])
+
+
 /**
  * 连接钱包
  */
-async function handleLink() {
-  walletDialogVisible.value = true
+async function handleLink(m) {
+  // not install弹窗
+  // installDialogVisible.value = true;
+  // 正在连接弹窗
+  connectDialogVisible.value = true
+
+
   // if (loadLink.value) return;
   // loadLink.value = true;
   // await appStore.linkWallet();
@@ -22,39 +74,83 @@ async function handleLink() {
 
 <template>
   <div class="wallet-wrap text-24">
-    <!-- 已链接钱包展示钱包地址 -->
-    <div v-if="appStore.defaultAccount" class="account-address">
-      {{ plusStar(appStore.defaultAccount, 4, 4) }}
-    </div>
-
-    <!-- 连接钱包 -->
-    <button v-load="loadLink" v-else class="connect-btn font-scr" @click="handleLink">
-      CONNECT WALLET
-    </button>
+    
+    <el-dropdown trigger="hover" popper-class="connect_wallet_btn" placement="bottom-start">
+      <!-- 已链接钱包展示钱包地址 -->
+      <div v-if="appStore.defaultAccount" class="account-address">
+        {{ plusStar(appStore.defaultAccount, 4, 4) }}
+      </div>
+      <!-- 连接钱包 -->
+      <button v-load="loadLink" v-else class="connect-btn font-scr">
+          CONNECT WALLET
+      </button>
+      <template #dropdown>
+        <div class="text-center text-25">CHOOSE WALLET</div>
+        <div
+          @click="handleLink(m)"
+          class="flex justify-around cursor-pointer menu-child-item hover:text-#FFD770 border-1 border border-#8c855b rounded-10 ml-10 mr-10 p-10"
+          v-for="m in walletList"
+          :key="m.id"
+        >
+          <img :src="m.src" class="ml-20" />
+          <span class="flex-1 text-15">{{ m.name }}</span>
+        </div>
+      </template>
+    </el-dropdown>
   </div>
   <el-dialog
-    v-model="walletDialogVisible"
+    v-model="connectDialogVisible"
     class="bg-#2b2b2b rounded-10"
+    width="500rem"
+    center
+    align-center
+    :show-close="false"
+    :close-on-click-modal="false"
+  >
+    <div class="text-white text-25 text-center">CHOOSE WALLET</div>
+    
+    <div class="flex flex-col items-center mt-10">
+      <img :src="OKX" class="m-20" />
+      <div class="text-white text-20">OKX WALLET</div>
+    </div>
+    <div class="flex justify-center items-center mt-20">
+      <el-icon color="#fff59c" size="30rem"><Clock /></el-icon>
+      <span class="text-20 ml-10 text-white">Connecting...</span>
+    </div>
+    <el-divider class="border-#8c855b" />
+    <button class="connect-btn font-scr w-full items-center text-white text-20" @click="connectDialogVisible = false">
+      Cancel
+    </button>
+  </el-dialog>
+  <el-dialog
+    v-model="installDialogVisible"
+    class="bg-#2b2b2b rounded-10 border-1 border border-#fff59c"
     width="500rem"
     align-center
   >
     <template #title>
-      <div class="text-white text-30">CHOOSE WALLET</div>
+      <div class="text-white text-20 flex"><el-icon color="#fff59c" class="mr-10"><WarningFilled /></el-icon>okx wallet not isntalled</div>
     </template>
-    
-    <button class="connect-btn font-scr w-full flex items-center mb-20 p-20 border-1 border border-#979797 rounded-10 bg-#414141" @click="handleLink">
-      <img :src="OKX" />
-      <span class="text-white text-20 ml-120">OKX WALLET</span>
-    </button>
-    <button class="connect-btn font-scr w-full flex items-center p-16 pl-20 border-1 border border-#979797 rounded-10 bg-#414141" @click="handleLink">
-      <img :src="UNISAT" />
-      <span class="text-white text-20 ml-105">UNISAT WALLET</span>
-    </button>
+    <div class="color-#bebebe">PLEASE CLICK HERE TO WISIT THE PRODUCT</div>
+    <div class="color-#bebebe">PAGE.AFTER IHSTALLIHG THE EXTEHSI0M,</div>
+    <div class="color-#bebebe">PLEASE REFRESH THE PAGE.</div>
   </el-dialog>
 </template>
 
+<style lang="scss">
+.connect_wallet_btn{
+    width: 300rem !important
+}
+.connect_wallet_btn{
+  .menu-child-item:not(:first-child){
+    margin-top: 10rem !important;
+  }
+}
+
+</style>
 
 <style lang="scss" scoped>
+
 .wallet-wrap {
   .account-address,
   .connect-btn {
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import VueI18n from '@/locales/i18n';
 import 'animate.css';
 import bpDirective from './utils/bpDirective';
 import watchUrl from './router/watchUrl';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 // 初始化一些东西
 import '@/utils/initRem';
@@ -22,6 +23,9 @@ import { bpThrottle } from './hooks/useDeb';
 const pinia = createPinia();
 
 const vueApp = createApp(App);
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  vueApp.component(key, component)
+}
 // 多语言翻译标记
 vueApp.config.globalProperties.$p = (param): string => param;
 
