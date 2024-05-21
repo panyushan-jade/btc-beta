31a24f4bc0bd24b14b7ce8ff62398382481457e7
choy <2970174582@qq.com>
2024-05-22 01:11:39 +0800

    merge

--- src/hooks/useInscriptions.ts
+++ src/hooks/useInscriptions.ts
@@ -6,15 +6,15 @@ const useInscriptions = () => {
     const getInscriptions = async () => {
         if (appStore.wallet === 'OKX') {
             try {
-                let res = await window.okxwallet.bitcoin.getInscriptions(0,50);
+                let res = await window.okxwallet.bitcoin.getInscriptions(0,199);
                 console.log(res.total);
                 
-                if(res.total > 50){
+                if(res.total > 199){
                     Inscriptions.value.push(...res.list)
                     let PromiseArr = []
-                    for(let i = 1; i <= res.total /50; i+=1){
+                    for(let i = 1; i <= res.total /199; i+=1){
                         
-                        PromiseArr.push(window.okxwallet.bitcoin.getInscriptions(i*50, 50))
+                        PromiseArr.push(window.okxwallet.bitcoin.getInscriptions(i*199, 199))
                     }
                     Promise.all(PromiseArr).then(resArr => resArr.map(item=> Inscriptions.value.push(...item.list)));
                     
