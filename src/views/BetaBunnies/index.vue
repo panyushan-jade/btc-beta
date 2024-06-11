2b194dfeefe42212e5af16cd01b5fe2614aa15be
panyushan <1193345664@qq.com>
2024-06-11 16:48:55 +0800

    pref

--- src/views/BetaBunnies/index.vue
+++ src/views/BetaBunnies/index.vue
@@ -1,27 +1,44 @@
 <script setup lang="ts">
 import { useAppStore } from "@/store/appStore";
-import Vote from './Vote.vue'
-import Community from './Community.vue';
-import Application from './Application.vue';
+import { useRoute } from "vue-router";
+import Vote from "./Vote.vue";
+import Community from "./Community.vue";
+import Application from "./Application.vue";
 const appStore = useAppStore();
-const ContentType = ref('Apply')
+const ContentType = ref("Apply");
+const route = useRoute();
 </script>
 
 <template>
   <div class="beta-bunnies-wrap">
-    <img
-      v-if="appStore.curDevice === 'phone'"
-      src="@img/betaBunnies/mob-bunnies.png"
-      alt=""
-      class="w-full"
-    />
-    <img v-else src="@img/betaBunnies/pc-bunnies.png" alt="" class="w-full" />
-    <router-view>
-    </router-view>
+    <template v-if="route.path == '/beta-bunnies/Vote'">
+      <img
+        v-if="appStore.curDevice === 'phone'"
+        src="@img/betaBunnies/mob-banner-betaBunnies.png"
+        alt=""
+        class="w-full"
+      />
+      <img
+        v-else
+        src="@img/betaBunnies/pc-bunnies-vote.png"
+        alt=""
+        class="w-full"
+      />
+    </template>
+    <template v-else>
+      <img
+        v-if="appStore.curDevice === 'phone'"
+        src="@img/betaBunnies/mob-bunnies.png"
+        alt=""
+        class="w-full"
+      />
+      <img v-else src="@img/betaBunnies/pc-bunnies.png" alt="" class="w-full" />
+    </template>
+    <router-view> </router-view>
     <!-- <Vote /> -->
 
     <!-- <Community /> 两套代码PC和mob -->
-    
+
     <!-- <Application /> -->
   </div>
 </template>
